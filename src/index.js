5c532e0a8701cf9846c6df5bcd49959d3a420017
Amelia Hill <amehi4yus@gmail.com>
2022-06-10 16:34:50 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,28 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import Amplify from "aws-amplify";
-import awsconfig from "./aws-exports";
-awsconfig.oauth.redirectSignIn = `${window.location.origin}/`;
-awsconfig.oauth.redirectSignOut = `${window.location.origin}/`;
-Amplify.configure(awsconfig);
+import awsconfig from '../aws-exports'
+import {Amplify} from 'aws-amplify'
+
+const configureAmplify = () => {
+  const urlsIn = awsconfig.oauth.redirectSignIn.split(',')
+  const urlsOut = awsconfig.oauth.redirectSignOut.split(',')
+  const currentHost = location.protocol + '//' + location.host
+  const isCurrentHost = (url) => url.includes(currentHost)
+  const newUrls = {
+    redirectSignIn: urlsIn.find(url => isCurrentHost(url)),
+    redirectSignOut: urlsOut.find(url => isCurrentHost(url))
+  }
+  Amplify.configure({
+    ...awsconfig,
+    oauth: {
+      ...awsconfig.oauth,
+      ...newUrls
+    }
+  })
+}
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
