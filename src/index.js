9054b88eb9e9b43e7ad02de205d68eeffdee84cb
Amelia Hill <amehi4yus@gmail.com>
2022-06-10 18:54:10 -0700

    Update redirectSignIn

--- src/index.js
+++ src/index.js
@@ -4,35 +4,44 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// import  Amplify from 'aws-amplify'
+// import config from './aws-exports'
+// // Amplify.configure(config)
+
 import  Amplify from 'aws-amplify'
-import config from './aws-exports'
-Amplify.configure(config)
+import awsConfig from './aws-exports';
 
-var urlsIn = config.oauth.redirectSignIn.split(",");
-var urlsOut = config.oauth.redirectSignOut.split(",");
-const oauth = {
-  domain: config.oauth.domain,
-  scope: config.oauth.scope,
-  redirectSignIn: config.oauth.redirectSignIn,
-  redirectSignOut: config.oauth.redirectSignOut,
-  responseType: config.oauth.responseType
-};
+const isLocalhost = Boolean(
+  window.location.hostname === "localhost" ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === "[::1]" ||
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
 
-var hasLocalhost  = (hostname) => Boolean(hostname.match(/localhost/) || hostname.match(/127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}/));
-var hasHostname   = (hostname) => Boolean(hostname.includes(window.location.hostname));
-var isLocalhost   = hasLocalhost(window.location.hostname);
-if (isLocalhost) {
-  urlsIn.forEach((e) =>   { if (hasLocalhost(e)) { oauth.redirectSignIn = e; }});
-  urlsOut.forEach((e) =>  { if (hasLocalhost(e)) { oauth.redirectSignOut = e; }});
+// Assuming you have two redirect URIs, and the first is for localhost and second is for production
+const [
+  localRedirectSignIn,
+  productionRedirectSignIn,
+] = awsConfig.oauth.redirectSignIn.split(",");
+
+const [
+  localRedirectSignOut,
+  productionRedirectSignOut,
+] = awsConfig.oauth.redirectSignOut.split(",");
+
+const updatedAwsConfig = {
+  ...awsConfig,
+  oauth: {
+    ...awsConfig.oauth,
+    redirectSignIn: isLocalhost ? localRedirectSignIn : productionRedirectSignIn,
+    redirectSignOut: isLocalhost ? localRedirectSignOut : productionRedirectSignOut,
+  }
 }
-else {
-  urlsIn.forEach((e) =>   { if (hasHostname(e)) { oauth.redirectSignIn = e; }});
-  urlsOut.forEach((e) =>  { if (hasHostname(e)) { oauth.redirectSignOut = e; }});
-}
-var configUpdate = config;
-configUpdate.oauth = oauth;
-Amplify.configure(configUpdate);
 
+Amplify.configure(updatedAwsConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -42,5 +51,5 @@ root.render(
 );
 
 
-serviceWorker.unregister();
+// serviceWorker.unregister();
 
